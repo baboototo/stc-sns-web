fa687b997918a1a1c473bf7b2841acba53fe8677
baboototo <baboototo@gmail.com>
2019-10-28 10:43:23 +0900

    미디어통계 SSL URL 적용

--- web/sns/js/common/licence.js
+++ web/sns/js/common/licence.js
@@ -2,9 +2,9 @@ var licence
 licence = licence || {};
 
 licence = {
-    // url: 'http://localhost:11422/api',
-    // url: 'http://175.207.12.220:11422/api',
-    url: 'http://175.125.91.228:11422/api',
+     url: 'http://localhost:11422/api',
+    // url: 'https://stclab.co.kr:11422/api',
+    // url: 'http://175.125.91.228:11422/api',
     licence: 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJTVEMiLCJVU0VSX05BTUUiOiJNQ1NUIiwiTElDRU5TRV9TIjoxNTY0OTMwODAwLCJMSUNFTlNFX0UiOjE1OTY0NjY4MDB9.Mx2gWlXZsYhtzfKiQD8rfBAMT4InvORqKe98ZAft5Hk'
 }
 
